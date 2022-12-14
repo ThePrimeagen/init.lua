4a96e6457b0a0241ca7361ce62177aa6b9a33a38
mpaulson <mpaulson@netflix.com>
2022-12-14 06:22:00 -0700

    feat: fugitive keymap updates
    chore
    
    testing
    
    noteuh:wq

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,31 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
+local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufWinEnter", {
+    group = ThePrimeagen_Fugitive,
+    pattern = "*",
+    callback = function()
+        print("help", vim.bo.ft)
+        if vim.bo.ft ~= "fugitive" then
+            return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = {buffer = bufnr, remap = false}
+        print("great success", vim.bo.ft, bufnr, vim.inspect(opts))
+        vim.keymap.set("n", "<leader>p", function()
+            vim.cmd [[ Git push ]]
+        end, opts)
+
+        -- rebase always
+        vim.keymap.set("n", "<leader>P", function()
+            vim.cmd [[ Git pull --rebase ]]
+        end, opts)
+
+        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+        -- needed if i did not set the branch up correctly
+        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+    end,
+})
