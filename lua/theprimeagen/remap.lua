04d7c04f7205414a919c4803e4fedf9655e53519
Yochem van Rosmalen <yochem+git@icloud.com>
2022-12-16 19:38:44 +0100

    vim.keymap.set accepts table of modes

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -22,12 +22,10 @@ end)
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set("n", "<leader>y", [["+y]])
-vim.keymap.set("v", "<leader>y", [["+y]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set("n", "<leader>d", [["_d]])
-vim.keymap.set("v", "<leader>d", [["_d]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
