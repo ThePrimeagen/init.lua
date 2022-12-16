168d0f152f954150c14dd1b7347c1ce8d876468c
Yochem van Rosmalen <yochem+git@icloud.com>
2022-12-16 19:47:23 +0100

    Consistant vim.cmd.<cmd> and remove unnecessary function wrapping

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -1,7 +1,7 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -32,9 +32,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", function()
-    vim.lsp.buf.format()
-end)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
