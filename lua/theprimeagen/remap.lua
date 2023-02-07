aaa94f12f3efd338eeb42667a68adef71fd9e6e0
mpaulson <the.primeagen@gmail.com>
2023-02-07 15:20:02 -0700

    feat: remaps

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -45,7 +45,7 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
-vim.keymap.set("n", "<leader>tt", function()
-    require("theprimeagen.typist").next(16)
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
 end)
 
