448350cfd7a07f7a41eaad626231ec110b197d65
pedromch <83724978+pedromch@users.noreply.github.com>
2023-02-13 22:38:04 -0300

    fix: change deprecated name sumneko_lua to lua_ls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,12 +4,12 @@ lsp.preset("recommended")
 
 lsp.ensure_installed({
   'tsserver',
-  'sumneko_lua',
+  'lua_ls',
   'rust_analyzer',
 })
 
 -- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
+lsp.configure('lua_ls', {
     settings = {
         Lua = {
             diagnostics = {
