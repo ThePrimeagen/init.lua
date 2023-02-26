85524545ef99dd55995c8251223724f749cd500e
Javier Flores <floresjavier09@outlook.com>
2023-02-26 15:48:57 -0300

    Add a safe-guard for old Powershell
    Use Windows Powershell in the case Powershell Core isn't available.
    
    Also change .o. to .opt. to match the style of the whole file

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -31,13 +31,20 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 if vim.loop.os_uname().sysname == 'Windows_NT' then
-    vim.o.shell = 'pwsh'
-    vim.o.shellcmdflag =
+
+    -- Use Powershell Core, if not available Windows Powershell
+    if vim.fn.executable('pwsh') == 1 then
+        vim.opt.shell = 'pwsh'
+    else
+        vim.opt.shell = 'powershell'
+    end
+
+    vim.opt.shellcmdflag =
     [[-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command
     [Console]::InputEncoding=[Console]::OutputEncoding =
     [System.Text.Encoding]::UTF8;]]
-    vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    vim.o.shellquote = ''
-    vim.o.shellxquote = ''
+    vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    vim.opt.shellquote = ''
+    vim.opt.shellxquote = ''
 end
