1b7edf156aa516c9894c55bb6a97b1712f7b2f52
Javier Flores <floresjavier09@outlook.com>
2023-02-26 15:04:28 -0300

    Modify defaults to use pwsh on Windows
    This makes Neovim use Powershell Core when on Windows for `:!` and
    `:term` commands.
    
    It's kind of opinionated but I believe it's better than defaulting to
    the old cmd.

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -30,3 +30,14 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+if vim.loop.os_uname().sysname == 'Windows_NT' then
+    vim.o.shell = 'pwsh'
+    vim.o.shellcmdflag =
+    [[-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command
+    [Console]::InputEncoding=[Console]::OutputEncoding =
+    [System.Text.Encoding]::UTF8;]]
+    vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    vim.o.shellquote = ''
+    vim.o.shellxquote = ''
+end
