b5ebe608b309457a2c93abb9af971b4aa70f730e
Afonso Lage <lage.afonso@gmail.com>
2023-02-27 07:11:06 -0300

    Dont loose bg waifu when PackerSync

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -16,9 +16,6 @@ return require('packer').startup(function(use)
   use({
 	  'rose-pine/neovim',
 	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
   })
 
   use({
