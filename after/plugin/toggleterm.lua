e0d8744ecc276184e9e7f7ac593369ea8245b638
zbluee <amanueltemesgen9@gmail.com>
2023-04-02 14:50:16 -0400

    Add toggleterm.lua configuration file with custom setup.

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,31 @@
+require("toggleterm").setup{
+  size = function(term)
+    if term.direction == "float" then
+      return 20
+    elseif term.direction == "horizontal" then
+      return 15
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+  end,
+  open_mapping = [[<c-\>]],
+  hide_numbers = true,
+  shade_filetypes = {},
+  shade_terminals = true,
+  shading_factor = 2,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  direction = "float",
+  close_on_exit = true,
+  shell = vim.o.shell,
+  float_opts = {
+    border = "curved",
+    winblend = 0,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    },
+  },
+}
+
