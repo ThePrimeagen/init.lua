7e994da10648a6390491e8a3d6e7b5bc0e71dfcd
Chris Stadler <chris.stadler@target.com>
2023-11-16 09:33:58 -0500

    Some personal vim opts

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -1,5 +1,3 @@
-vim.opt.guicursor = ""
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -28,5 +26,5 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "120"
 
