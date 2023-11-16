6bfd9060ffad17c305b3a0aa919fbd9ea69893fa
Chris Stadler <chris.stadler@target.com>
2023-11-16 10:27:00 -0500

    Personalize key mappins

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,11 +2,11 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>6", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>7", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>8", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>9", function() ui.nav_file(4) end)
 
 
--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -1,27 +1,37 @@
-
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- ii to Esc
+vim.keymap.set("i", "ii", "<Esc>")
+
+-- File Elxplorer
+vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
+
+-- Highlight and move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Append lines keeping cursor at beginning
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep search terms in the middle
+--vim.keymap.set("n", "<C-d>", "<C-d>zz")
+--vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
+--vim.keymap.set("n", "<leader>vwm", function()
+--    require("vim-with-me").StartVimWithMe()
+--end)
+--vim.keymap.set("n", "<leader>svwm", function()
+--    require("vim-with-me").StopVimWithMe()
+--end)
 
 -- greatest remap ever
+-- Paste into highlight without losing whats in clipboard
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
+-- Allow for copy/paste using system clipboard too
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
@@ -32,19 +42,21 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>l", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- Find the word the cursor is currently on and insert everywhere
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
+-- Shout it out
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
