d17c0d739be4e482ccb9943120db283cba277647
Chris Stadler <chris.stadler@target.com>
2023-11-16 10:11:03 -0500

    Remove the line Prime told me to

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -1,10 +1,6 @@
 require("theprimeagen.set")
 require("theprimeagen.remap")
 
--- DO NOT INCLUDE THIS
-vim.opt.rtp:append("~/personal/streamer-tools")
--- DO NOT INCLUDE THIS
-
 local augroup = vim.api.nvim_create_augroup
 local ThePrimeagenGroup = augroup('ThePrimeagen', {})
 
