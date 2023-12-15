cda329b096e7b2d9247f574e397dee8c3e87eae7
Ujjaval Parmar <71434743+UjjavalParmar@users.noreply.github.com>
2023-12-15 22:03:46 +0530

    telescope 1.5

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -6,7 +6,6 @@ vim.cmd.packadd('packer.nvim')
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	  -- or                            , branch = '0.1.x',
