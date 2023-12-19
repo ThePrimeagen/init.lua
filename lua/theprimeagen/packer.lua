5d0e3ea3a9f095b83f17ceec00e9cd150cf1ca05
Amir Farhadi <farhaadi.amir@gmail.com>
2023-12-19 05:23:43 -0500

    Fix: Updated telescope version
    this fixes the "invalid event user telescopefindpre" error
    issue: https://github.com/nvim-telescope/telescope.nvim/issues/2735

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
