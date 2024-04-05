3549b17d7847c72131f4085226367ccbc8a202f0
theprimeagen <theprimeagen@gmail.com>
2024-04-05 11:14:41 -0600

    ohhh lazy868

--- lua/theprimeagen/lazy/lsp.lua
+++ lua/theprimeagen/lazy/lsp.lua
@@ -28,6 +28,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
+                "gopls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
