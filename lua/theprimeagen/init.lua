ee4585831474628a9b27e8b8d35fb12ebcd400ea
george looshch <looshch@loosh.ch>
2024-07-03 12:30:35 +0100

    chore: vim.lsp.buf.hover() key map
    Remove vim.lsp.buf.hover() key map as it’s built-in since Neovim v0.10.

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -51,7 +51,6 @@ autocmd('LspAttach', {
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
