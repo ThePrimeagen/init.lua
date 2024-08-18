53b20a9c3532a2e64aacbfb092a251dcebb37628
André Azevedo <andre.azev@gmail.com>
2024-08-18 16:58:05 -0300

    feat: added tailwindcss LSP;

--- lua/theprimeagen/lazy/lsp.lua
+++ lua/theprimeagen/lazy/lsp.lua
@@ -28,6 +28,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
+                "tailwindcss",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
