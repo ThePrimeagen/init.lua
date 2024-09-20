6e8561620796d227fe5590dee5fb272fc5eed688
André Azevedo <andre.azev@gmail.com>
2024-09-20 13:07:44 -0300

    Added buffer clean keymap;

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -1,4 +1,3 @@
-
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
@@ -55,3 +54,4 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
+vim.keymap.set("n", "<leader>bd", "<cmd>%bd|e#|bd#<cr>|'\"")
