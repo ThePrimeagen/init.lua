acfe89b11d79e3e638652e81465e6ca624a7dbea
Jayant Shrivastava <jayant.shrivastava@datadoghq.com>
2024-10-11 15:24:50 -0400

    add go to usages (does not support go interfaces)

--- lua/theprimeagen/lazy/telescope.lua
+++ lua/theprimeagen/lazy/telescope.lua
@@ -25,6 +25,8 @@ return {
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+        vim.keymap.set('n', '<leader>g', builtin.lsp_references, {})
     end
 }
 
