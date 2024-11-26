64a5d36033d242a3fbd10965dfbfcb05cca49dd0
André Azevedo <andre.azev@gmail.com>
2024-11-26 18:56:16 -0300

    feat: moved away from ts_ls in favour of typescript-tools;

---
+++ lua/theprimeagen/lazy/typescript-tools.lua
@@ -0,0 +1,25 @@
+return {
+    "pmizio/typescript-tools.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        require("typescript-tools").setup {
+            settings = {
+                tsserver_max_memory = 8192,
+                separate_diagnostic_server = false,
+                code_lens = "off",
+                jsx_close_tag = {
+                    enable = false,
+                    filetypes = { "typescriptreact", "javascriptreact" },
+                },
+                tsserver_plugins = {
+                    "@styled/typescript-styled-plugin",
+                }
+            },
+        }
+    end,
+    opts = {
+    },
+}
