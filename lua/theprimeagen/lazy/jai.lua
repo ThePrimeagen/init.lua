5c4207ba099926b3e1f4147ed23bdea82f1b98e0
Prime Again <the.primeagain@gmail.com>
2025-01-15 08:54:11 -0700

    automated dev commit

---
+++ lua/theprimeagen/lazy/jai.lua
@@ -0,0 +1,4 @@
+return {
+    "rluba/jai.vim",
+}
+
---
+++ lua/theprimeagen/lazy/peek.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "toppair/peek.nvim",
+        event = { "VeryLazy" },
+        build = "deno task --quiet build:fast",
+        config = function()
+            require("peek").setup({
+                filetype = { 'markdown', 'conf' }
+            })
+            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        end,
+    },
+}
