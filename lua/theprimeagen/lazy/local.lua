6a1be2f2a35621e5b4b9e3d76f83c90de7d5e5d3
Prime Again <the.primeagain@gmail.com>
2025-02-02 12:57:28 -0700

    automated dev commit

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -5,6 +5,8 @@ local local_plugins = {
         dir = "~/personal/cockpit",
         config = function()
             require("cockpit")
+            vim.keymap.set("n", "<leader>ct", "<cmd>CockpitTest<CR>")
+            vim.keymap.set("n", "<leader>cr", "<cmd>CockpitRefresh<CR>")
         end,
     },
 
