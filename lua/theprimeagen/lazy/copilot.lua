d3c79a7a797d5db3e70f0215f852d646ea7fc18f
AhmedKaram <52092726+Adamkaram@users.noreply.github.com>
2025-02-22 06:07:11 +0200

    Update copilot.lua
    try this prime

--- lua/theprimeagen/lazy/copilot.lua
+++ lua/theprimeagen/lazy/copilot.lua
@@ -2,24 +2,47 @@ return {
     {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
+        build = ":Copilot auth",
         event = "InsertEnter",
         config = function()
             require("copilot").setup({
+                panel = {
+                    enabled = true,
+                    auto_refresh = true,
+                    keymap = {
+                        jump_next = "<c-j>",
+                        jump_prev = "<c-k>",
+                        accept = "<c-a>",
+                        refresh = "r",
+                        open = "<M-CR>",
+                    },
+                    layout = {
+                        position = "bottom", -- | top | left | right
+                        ratio = 0.4,
+                    },
+                },
                 suggestion = {
                     enabled = true,
                     auto_trigger = true,
-                    hide_during_completion = false,
-                    debounce = 25,
+                    debounce = 75,
                     keymap = {
-                        accept = false,
+                        accept = "<c-a>",
                         accept_word = false,
-                        accept_line = "<Tab>",
-                        next = false,
-                        prev = false,
-                        dismiss = false,
+                        accept_line = false,
+                        next = "<c-j>",
+                        prev = "<c-k>",
+                        dismiss = "<C-e>",
                     },
                 },
             })
         end,
     },
+
+    -- {
+    --     "zbirenbaum/copilot-cmp",
+    --     -- after = { "copilot.lua" },
+    --     config = function()
+    --         require("copilot_cmp").setup()
+    --     end,
+    -- }
 }
