158c9ccd652e5921cc6940205da6ed20776e7cc7
Primeagain <prime@prime.prime>
2025-05-09 14:25:30 -0600

    automated dev commit

--- lua/theprimeagen/lazy/conform.lua
+++ lua/theprimeagen/lazy/conform.lua
@@ -5,6 +5,7 @@ return {
         require("conform").setup({
             formatters_by_ft = {
                 lua = { "stylua" },
+                go = { "gofmt" }
             }
         })
     end
