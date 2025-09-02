78fe7448e98707d1e787ed5e8ef03367132fc103
Primeagain <prime@prime.prime>
2025-09-02 14:56:54 -0600

    automated dev commit

--- lua/theprimeagen/lazy/conform.lua
+++ lua/theprimeagen/lazy/conform.lua
@@ -1,16 +1,27 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {},
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                go = { "gofmt" },
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                elixir = { "mix" }
-            }
-        })
-    end
-}
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				lua = { "stylua" },
+				go = { "gofmt" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				elixir = { "mix" },
+			},
+            formatters = {
+                ["clang-format"] = {
+                    prepend_args = { "-style=file", "-fallback-style=LLVM" },
+                },
+            },
+		})
 
+        vim.keymap.set("n", "<leader>f", function()
+            require("conform").format({ bufnr = 0 })
+        end)
+
+	end,
+}
