f414afa04f3e765c890705bfe7b44bf6a5b1bbcf
Jayant Shrivastava <jayant.shrivastava@datadoghq.com>
2025-11-15 14:08:02 -0500

    harpoon

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -19,6 +19,16 @@ local local_plugins = {
             vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
             vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
             vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
+
+            -- Quick menu number keys
+            harpoon:extend({
+                UI_CREATE = function(cx)
+                    vim.keymap.set("n", "1", function() harpoon:list():select(1) end, { buffer = cx.bufnr })
+                    vim.keymap.set("n", "2", function() harpoon:list():select(2) end, { buffer = cx.bufnr })
+                    vim.keymap.set("n", "3", function() harpoon:list():select(3) end, { buffer = cx.bufnr })
+                    vim.keymap.set("n", "4", function() harpoon:list():select(4) end, { buffer = cx.bufnr })
+                end,
+            })
         end
     },
     {
