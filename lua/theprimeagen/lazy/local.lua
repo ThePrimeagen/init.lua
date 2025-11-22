489cfce626ecbbf00362c83d8f9b408fe7b1ebc7
ThePrimeagen <prime@prime.prime>
2025-11-22 11:39:15 -0800

    dev commit

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -1,10 +1,10 @@
 local local_plugins = {
-	--{
-	--    "vim-guys",
-	--    dir = "~/personal/vim-guys",
-	--    config = function()
-	--    end,
-	--},
+	{
+	   "dreamy",
+	   dir = "~/personal/dreamy",
+	   config = function()
+	   end,
+	},
 	--{
 	--    "cockpit",
 	--    dir = "~/personal/cockpit",
@@ -19,54 +19,7 @@ local local_plugins = {
 		"the-stru",
 		dir = "~/personal/the-stru",
 	},
-	{
-		"cursor_agent",
-		dir = "~/personal/cursor_in_neovim/",
-		dependencies = { "nvim-lua/plenary.nvim" }, -- optional, improves HTTP; falls back to curl if absent
-		config = function()
-			require("cursor_agent").setup({
-				-- optional config
-				ui = {
-					width = 60, -- panel width in columns
-					input_height = 3, -- input box height
-					border = "rounded",
-				},
-				provider = {
-					name = "openai", -- or "anthropic"
-					openai = {
-						model = "gpt-4o-mini",
-					},
-					anthropic = {
-						model = "claude-3-5-sonnet-20240620",
-						version = "2023-06-01",
-						max_tokens = 1024,
-					},
-				},
-			})
-		end,
-	},
 
-	{
-		"streamer",
-		dir = "~/personal/eleven-streamer",
-		config = function()
-			vim.keymap.set("n", "<leader>er", function()
-				require("streamer").reload()
-			end)
-			vim.keymap.set("n", "<leader>es", function()
-				require("streamer").twitch_dashboard()
-			end)
-			vim.keymap.set("n", "<leader>en", function()
-				require("streamer").twitch_dashboard():stop()
-			end)
-		end,
-	},
-
-	{
-		"caleb",
-		dir = "~/personal/caleb",
-		config = function() end,
-	},
 	{
 		"harpoon",
 		dir = "~/personal/harpoon",
@@ -111,17 +64,7 @@ local local_plugins = {
 			end)
 		end,
 	},
-	{
-		"vim-apm",
-		dir = "~/personal/vim_apm",
-		config = function() end,
-	},
 
-	{
-		"vim-with-me",
-		dir = "~/personal/vim-with-me",
-		config = function() end,
-	},
 }
 
 return local_plugins
--- lua/theprimeagen/lazy/supermaven.lua
+++ lua/theprimeagen/lazy/supermaven.lua
@@ -1,9 +1,9 @@
 return {
-    {
-      "supermaven-inc/supermaven-nvim",
-      config = function()
-        require("supermaven-nvim").setup({})
-      end,
-    },
+    -- {
+    --   "supermaven-inc/supermaven-nvim",
+    --   config = function()
+    --     require("supermaven-nvim").setup({})
+    --   end,
+    -- },
 }
 
