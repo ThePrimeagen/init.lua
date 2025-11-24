f4d6b1bd516d61067c2ecd4eb6c187d51073f02f
ThePrimeAgain <theprimeagain@theprimeagain.com>
2025-11-24 11:14:58 -0700

    read the sweet sweet docs and realize its in neovim. no treesitter plugin

--- lua/theprimeagen/lazy/treesitter.lua
+++ lua/theprimeagen/lazy/treesitter.lua
@@ -54,24 +54,6 @@ return {
 					-- Instead of true it can also be a list of languages
 					additional_vim_regex_highlighting = { "markdown" },
 				},
-				playground = {
-					enable = true,
-					disable = {},
-					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-					persist_queries = false, -- Whether the query persists across vim sessions
-					keybindings = {
-						toggle_query_editor = "o",
-						toggle_hl_groups = "i",
-						toggle_injected_languages = "t",
-						toggle_anonymous_nodes = "a",
-						toggle_language_display = "I",
-						focus_language = "f",
-						unfocus_language = "F",
-						update = "R",
-						goto_node = "<cr>",
-						show_help = "?",
-					},
-				},
 			})
 
 			local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
