15d712989b820e23cdd9e68c580cc8703393fea2
Jayant Shrivastava <jayant.shrivastava@datadoghq.com>
2025-12-19 21:46:25 -0500

    add git signs preview and revert

--- lua/theprimeagen/lazy/gitsigns.lua
+++ lua/theprimeagen/lazy/gitsigns.lua
@@ -1,6 +1,16 @@
 return {
     "lewis6991/gitsigns.nvim",
     config = function()
-        require('gitsigns').setup()
+        local gs = require("gitsigns")
+
+        gs.setup({
+            preview_config = {
+                border = "rounded",
+                style = "minimal",
+            },
+        })
+
+        vim.keymap.set("n", "<leader>g", gs.preview_hunk, { desc = "Gitsigns: preview hunk" })
+        vim.keymap.set("n", "<leader>r", gs.reset_hunk, { desc = "Gitsigns: reset hunk" })
     end
 }
