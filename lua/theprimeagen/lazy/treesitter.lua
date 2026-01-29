22d0d31cf23f298c029934411d352d1e1e0764ad
theprimeagain <the.primeagen@gmail.com>
2026-01-29 07:44:00 -0700

    main -> master /

--- lua/theprimeagen/lazy/treesitter.lua
+++ lua/theprimeagen/lazy/treesitter.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+        branch = "master",
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				-- A list of parser names, or "all"
